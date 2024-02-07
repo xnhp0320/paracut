91504e836d8d6ed7accfb983116b1f80958eec74
Peng He <xnhp0320@gmail.com>
2024-02-07 23:15:24 +0800

    add genSegs.

--- main.zig
+++ main.zig
@@ -230,11 +230,170 @@ test {
     try expect(list.items[0].weight == 2);
 }
 
+test {
+    _ = ParaTree;
+}
+
 const ParaTree = struct {
     root: ParaNode,
 
-    fn genSegs() void {
+    const Order = std.math.Order;
+    fn lessThan(_ : void, a: u32, b: u32) Order {
+        return std.math.order(a, b);
+    }
 
+    const PQlt = std.PriorityQueue(u32, void, lessThan);
+
+    fn genNonOverlappedSegs(seg_list: *std.ArrayList(Seg)) !std.ArrayList(Seg) {
+        var list = std.ArrayList(Seg).init(allocator);
+        var h = PQlt.init(allocator, {});
+        const items = seg_list.items;
+
+
+        var idx:usize = 1;
+        var curr:u32 = items[0].range.lo;
+        try h.add(items[0].range.hi);
+
+        while (idx < items.len) : (idx += 1) {
+            const r = &items[idx].range;
+            if (r.lo < h.peek().?) {
+                // We should be careful about the [A, A] (the hi and lo are equal) and [A, B].
+                // We need the results to be [A, A] and [A+1, B].
+                if (r.lo == curr and r.lo == r.hi) {
+                    try list.append(Seg{ .range = .{ .lo = curr, .hi = r.hi } });
+                    curr = r.hi + 1;
+                } else {
+                    if (curr < r.lo) {
+                        try list.append(Seg{ .range = .{ .lo = curr, .hi = r.lo - 1} });
+                        curr = r.lo;
+                    }
+                    try h.add(items[idx].range.hi);
+                }
+            } else {
+                while (h.peek()) |top| {
+                    // r.lo >= top
+                    if (r.lo == top) {
+
+                        // we need a seg: [r.lo, top], as the current range r overlaps with a
+                        // previous range (in the heap).
+                        if (r.lo > curr) {
+                            try list.append(Seg{.range = .{.lo = curr, .hi = r.lo - 1}});
+                            curr = r.lo;
+                        }
+
+                        // [B, B] and [A, B], we needs a [B, B] and [A, B-1]
+                        if (curr == top) {
+                            // if r.lo == curr == top, we need a [r.lo, top]
+                            // we will only add once in case there are multiple same `top'
+                            try list.append(Seg{.range = .{ .lo = r.lo, .hi = top }});
+                            curr = r.lo + 1;
+                        }
+                    } else if (r.lo > top) {
+                        // r.lo > top, and since there are values in heap, there are overlapped
+                        // ranges.
+                        if (curr < top) {
+                            try list.append(Seg{.range = .{ .lo = curr, .hi = top} });
+                            curr = top + 1;
+                        }
+                    } else {
+                        break;
+                    }
+
+                    _ = h.remove();
+                }
+
+                if (h.count() == 0 and curr < r.lo) {
+                    curr = r.lo;
+                }
+                try h.add(items[idx].range.hi);
+            }
+        }
+
+        while (h.peek()) |top| {
+            if (curr <= top) {
+                try list.append(Seg{.range = .{ .lo = curr, .hi = top }});
+                curr = top + 1;
+            }
+            _ = h.remove();
+        }
+        return list;
+    }
+
+    test {
+        var l = std.ArrayList(Seg).init(allocator);
+        defer l.deinit();
+        // [0, 0] and [0, 1]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 0}});
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 1}});
+        var g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 0 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 1, .hi = 1 }}));
+        try expect(g.items.len == 2);
+        g.deinit();
+        l.clearAndFree();
+
+
+        // [0, 1] and [0, 0]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 1}});
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 0}});
+        g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 0 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 1, .hi = 1 }}));
+        try expect(g.items.len == 2);
+        g.deinit();
+        l.clearAndFree();
+
+        // [0, 1] and [1, 1]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 1}});
+        try l.append(Seg{ .range = .{ .lo = 1, .hi = 1}});
+        g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 0 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 1, .hi = 1 }}));
+        try expect(g.items.len == 2);
+        g.deinit();
+        l.clearAndFree();
+
+        // [0, 3], [1, 2]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 3}});
+        try l.append(Seg{ .range = .{ .lo = 1, .hi = 2}});
+        g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 0 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 1, .hi = 2 }}));
+        try expect(g.items[2].eq(Seg{ .range = .{ .lo = 3, .hi = 3 }}));
+        try expect(g.items.len == 3);
+        g.deinit();
+        l.clearAndFree();
+
+        // [0, 1], [2, 3]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 1}});
+        try l.append(Seg{ .range = .{ .lo = 2, .hi = 3}});
+        g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 1 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 2, .hi = 3 }}));
+        try expect(g.items.len == 2);
+        g.deinit();
+        l.clearAndFree();
+
+        // [0, 1], [3, 5]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 1}});
+        try l.append(Seg{ .range = .{ .lo = 3, .hi = 5}});
+        g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 1 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 3, .hi = 5 }}));
+        try expect(g.items.len == 2);
+        g.deinit();
+        l.clearAndFree();
+
+        // [0, 4], [3, 5]
+        try l.append(Seg{ .range = .{ .lo = 0, .hi = 4}});
+        try l.append(Seg{ .range = .{ .lo = 3, .hi = 5}});
+        g = try genNonOverlappedSegs(&l);
+        try expect(g.items[0].eq(Seg{ .range = .{ .lo = 0, .hi = 2 }}));
+        try expect(g.items[1].eq(Seg{ .range = .{ .lo = 3, .hi = 4 }}));
+        try expect(g.items[2].eq(Seg{ .range = .{ .lo = 5, .hi = 5 }}));
+        try expect(g.items.len == 3);
+        g.deinit();
+        l.clearAndFree();
     }
 
 
